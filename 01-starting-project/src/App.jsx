eb888e4abe7ef0b5ead44f71c9db1018ab6538d4
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-11-26 05:58:24 +0530

    adding Several types of Props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,38 +1,51 @@
-import reactImg from './assets/react-core-concepts.png'
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS,EXAMPLES } from "./data";
 
-const reactDesciption=['Fundamental','Crucial','Core'];
+const reactDesciption = ["Fundamental", "Crucial", "Core"];
 
-function getRandomInt(max){
-  return Math.floor(Math.random()*(max + 1));
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 
-function Header(){
-  const description= reactDesciption[getRandomInt(2)];
-  return(<header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      Fundamental React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>);
+function Header() {
+  const description = reactDesciption[getRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
 }
 
-function CoreConcept(){
-  return(
+function CoreConcept({title,description,image}) {
+  return (
     <li>
-      <img src="..." alt="..." />
-      <h3>Title</h3>
-      <p>Description</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
-  )
+  );
 }
 
 function App() {
-  return (
+ return (
     <div>
-      <Header/>
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
