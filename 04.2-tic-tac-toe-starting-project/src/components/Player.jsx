5aca64a42f92f32556a36daaa9f9f0312587152e
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-11-28 05:28:05 +0530

    Working With state and Instance Work In Isolation

--- 04.2-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04.2-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Player({name, symbol}) {
+function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
   return (
     <li>
-            <span className="player">
-              <span className="player-name">{name}</span>
-              <span className="player-symbol">{symbol}</span>
-            </span>
-            <button>Edit</button>
-          </li>
-  )
+      <span className="player">
+        {isEditing ? <input type="text" required/> : <span className="player-name">{name}</span>}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing?"Edit":"Save"}</button>
+    </li>
+  );
 }
 
-export default Player
+export default Player;
