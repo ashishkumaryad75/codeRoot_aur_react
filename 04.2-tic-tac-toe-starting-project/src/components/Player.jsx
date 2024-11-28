3dc2b9dd1a7caf68593b33a3e868874e4d563ae4
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-11-28 05:42:00 +0530

    Conditional Content & A Suboptimal Way Of Updating State And Updating State Based On Old State Correctly

--- 04.2-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04.2-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,16 +3,30 @@ import React, { useState } from "react";
 function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleName(e) {
+    // TODO: Handle The input
+    console.log(e.target.value);
   }
 
   return (
     <li>
       <span className="player">
-        {isEditing ? <input type="text" required/> : <span className="player-name">{name}</span>}
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => handleName(e)}
+          />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{!isEditing?"Edit":"Save"}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
