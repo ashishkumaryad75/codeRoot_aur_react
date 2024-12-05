87f33fb573c208ae6fd33b59b1f228665fac3780
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-05 20:31:45 +0530

    restart the game and why immutibility maters

--- 04.2-tic-tac-toe-starting-project/src/components/GameOver.jsx
+++ 04.2-tic-tac-toe-starting-project/src/components/GameOver.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-function GameOver({winner}) {
+function GameOver({ winner, onRestart }) {
   return (
     <>
       <div id="game-over">
         <h2>Game Over</h2>
         {winner && <p>{winner} Won!</p>}
         {!winner && <p>It&apos;s a drow!</p>}
-        <p><button>Rematch!</button></p>
+        <p>
+          <button onClick={onRestart}>Rematch!</button>
+        </p>
       </div>
     </>
   );
