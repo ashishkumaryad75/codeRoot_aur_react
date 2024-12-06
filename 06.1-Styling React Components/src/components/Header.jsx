ce9bf7ce28de5ce7d5396d98496359c7ff5a0bbc
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-06 19:50:21 +0530

    styling react app with inline style

--- 06.1-Styling React Components/src/components/Header.jsx
+++ 06.1-Styling React Components/src/components/Header.jsx
@@ -1,12 +1,19 @@
-import logo from '../assets/logo.png';
-import './Header.css'
+import logo from "../assets/logo.png";
+import "./Header.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p
+        style={{
+          color: "white",
+          fontSize: "25px",
+        }}
+      >
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
