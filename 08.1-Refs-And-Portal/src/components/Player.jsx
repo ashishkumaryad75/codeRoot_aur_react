6f41df9dcd2d338b0176c7f1602432b2f72ead84
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-08 23:08:18 +0530

    Managing User Input with State (Two Way Buinding)

--- 08.1-Refs-And-Portal/src/components/Player.jsx
+++ 08.1-Refs-And-Portal/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState(null);
+  const [submitted, setsubmitted] = useState(false);
+
+  function handleChange(event) {
+    setsubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setsubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : " unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
