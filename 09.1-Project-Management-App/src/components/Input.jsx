ff3269547d50f34df1ccec3e3c0609bc7bd28e1a
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-12 00:00:05 +0530

    styling button and input with tailwind css

--- 09.1-Project-Management-App/src/components/Input.jsx
+++ 09.1-Project-Management-App/src/components/Input.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
 function Input({ label, textarea, ...props }) {
+const classes="w-full p-1 border-b-2 rounded-sm broder-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
   return (
     <>
-      <p>
-        <label htmlFor="">{label}</label>
+      <p className="flex flex-col gap-1 my-4">
+        <label
+          htmlFor=""
+          className="text-sm font-bold uppercase text-stone-500"
+        >
+          {label}
+        </label>
       </p>
-      {textarea ? <textarea {...props}/> : <input {...props}/>}
+      {textarea ? (
+        <textarea
+          className={classes}
+          {...props}
+        />
+      ) : (
+        <input className={classes} {...props} />
+      )}
     </>
   );
 }
