4b188ee7245e3889236d44bcaee87ca7b0f36399
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-15 18:06:09 +0530

    different way to consume the Context

--- 10.1-ContextAPI-and-useReducer/src/components/CartModal.jsx
+++ 10.1-ContextAPI-and-useReducer/src/components/CartModal.jsx
@@ -1,6 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
-import Cart from './Cart';
+import Cart, { Cart1 } from './Cart';
 
 const CartModal = forwardRef(function Modal(
   { cartItems, onUpdateCartItemQuantity, title, actions },
@@ -20,6 +20,7 @@ const CartModal = forwardRef(function Modal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
       <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      {/* <Cart1 items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} /> */}
       <form method="dialog" id="modal-actions">
         {actions}
       </form>
