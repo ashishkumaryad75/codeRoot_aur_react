82bec858013acc0d44b5db4b1913a85c573d5159
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-16 22:16:13 +0530

    Prepairing Another Problem That can be Fixed with useEffect.

--- 11.1-Handling-Side-Effects/src/App.jsx
+++ 11.1-Handling-Side-Effects/src/App.jsx
@@ -69,6 +69,10 @@ function App() {
   return (
     <>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        {/* {modalIsOpen && <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />}  */}
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
