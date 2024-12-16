372df90a722d5e27f7bfcdc1f30756453c3a0a60
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-17 01:31:42 +0530

    using Effect Cleanup functions and using keys for resetting Components

--- 12.1-Quiz-App/src/components/QuestionTimer.jsx
+++ 12.1-Quiz-App/src/components/QuestionTimer.jsx
@@ -5,21 +5,24 @@ function QuestionTimer({ timeout, onTimeout }) {
 
   useEffect(() => {
     console.log("SETTING TIMEOUT");
-    
-    setTimeout(onTimeout, timeout);
+
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    // const interval = 
     console.log("SETTING INTERVAL");
-    
-    setInterval(() => {
+
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
 
-    // return () => {
-    //   clearInterval(interval);
-    // };
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
