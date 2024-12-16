365e7335df7b26c93efc82e3c371dfa9c56a66cd
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-16 22:37:18 +0530

    the problem with object and functional dependencies with example

--- 11.1-Handling-Side-Effects/src/App.jsx
+++ 11.1-Handling-Side-Effects/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    seModalIsOpen(false);
+    // seModalIsOpen(false);
 
     const storedId = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
     localStorage.setItem(
