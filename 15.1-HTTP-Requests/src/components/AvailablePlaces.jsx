22ba67839f00a442b1a18c86852a60036321f1b9
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-25 05:31:43 +0530

    Handling the errors for http requests

--- 15.1-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 15.1-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -1,20 +1,41 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/placesss");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          // it is ok then return status code 200, 300..
+          throw new Error("Failed to fetch the places...");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        // error..
+        setError({
+          message:
+            error.message || "Could not get the data, please try again later..",
+        });
+      }
+
       setIsFetching(false);
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An Error Occured!!!" message={error.message} />;
+  }
 
   return (
     <Places
