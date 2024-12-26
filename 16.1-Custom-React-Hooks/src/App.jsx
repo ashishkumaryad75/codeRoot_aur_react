8f060fbdff642c62dc860ffeb2d213af386b0a43
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2024-12-26 16:22:39 +0530

    Custom Hook : Managing State and Returning State Values & Exposing Nested Function From The Custom Hook.

--- 16.1-Custom-React-Hooks/src/App.jsx
+++ 16.1-Custom-React-Hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback, useEffect } from "react";
+import { useRef, useState, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import Modal from "./components/Modal.jsx";
@@ -7,20 +7,21 @@ import logoImg from "./assets/logo.png";
 import AvailablePlaces from "./components/AvailablePlaces.jsx";
 import { fetchUserPlaces, updateUserPlaces } from "./http.js";
 import Error from "./components/Error.jsx";
-import useFetch from "./hooks/useFetch.jsx";
+import { useFetch } from "./hooks/useFetch.jsx";
 
 function App() {
   const selectedPlace = useRef();
-
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
 
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useFetch();// custom hook...
+  const {
+    isFetching,
+    fetchedData: userPlaces,
+    setFetchedData: setUserPlaces,
+    error,
+  } = useFetch(fetchUserPlaces, []); // custom hook...
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -75,7 +76,7 @@ function App() {
 
       setModalIsOpen(false);
     },
-    [userPlaces]
+    [userPlaces, setUserPlaces]
   );
 
   function handleError() {
