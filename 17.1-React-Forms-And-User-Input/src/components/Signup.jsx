4f47b8a55a4cbb4f52bf2091203cfd9971e2e833
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-05 01:56:29 +0530

    Getting values via FormData and NAtive Browser Apis

--- 17.1-React-Forms-And-User-Input/src/components/Signup.jsx
+++ 17.1-React-Forms-And-User-Input/src/components/Signup.jsx
@@ -1,6 +1,19 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("submitted");
+
+    const fd = new FormData(event.target);
+    // const enterdEmail =fd.get('email');
+    // const enterdPassword =fd.get('email');
+    const acquisitionChannel = fd.getAll('acquisition')
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisitionChannel;
+    console.log(data);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
