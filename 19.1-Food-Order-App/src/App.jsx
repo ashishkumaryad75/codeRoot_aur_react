7c02f7f5e52bc585638960c0c200ae3792f0f05d
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-07 17:26:27 +0530

    Fetching the Meals data (GET HTTP Request)

--- 19.1-Food-Order-App/src/App.jsx
+++ 19.1-Food-Order-App/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
-// import Meals from "./Meals";
+import Meals from "./Meals";
 
 function App() {
   return (
     <>
      <Header/>
-     {/* <Meals/> */}
+     <Meals/>
     </>
   );
 }
