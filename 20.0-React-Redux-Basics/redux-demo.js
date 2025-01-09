235f7745f2d6f77477f96f115e1d249e199552ee
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-09 22:12:49 +0530

    Understanding the redux by using node.js with example.

---
+++ 20.0-React-Redux-Basics/redux-demo.js
@@ -0,0 +1,33 @@
+console.log("Redux");
+
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  // creating the reducer function
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 5,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 2,
+    };
+  }
+
+  return state; // returning the state
+};
+
+const store = redux.createStore(counterReducer); // creating the store
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+}; // create a counter Subscription
+
+store.subscribe(counterSubscriber); // subscribe the counterSubscriber from the store
+
+store.dispatch({ type: "increment" }); // dispatching the action for increment
+
+store.dispatch({ type: "decrement" }); // dispatching the action for decrement
