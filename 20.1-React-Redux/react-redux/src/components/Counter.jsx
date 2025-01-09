86f3facc219efa24e89a88522e98953d9c1de521
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-10 02:09:03 +0530

    Attaching the payload to the Actions

--- 20.1-React-Redux/react-redux/src/components/Counter.jsx
+++ 20.1-React-Redux/react-redux/src/components/Counter.jsx
@@ -15,12 +15,17 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   }
 
+  function increaseHandle(){
+    dispatch({ type: "increase",amount:5 });
+  }
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandle}>Increment</button>
+        <button onClick={increaseHandle}>Incresed by 5</button>
         <button onClick={decrementHandle}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
