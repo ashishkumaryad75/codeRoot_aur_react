1745565cb83ab4e7a031741b87f7ca0a7ec96284
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-13 23:57:40 +0530

    Navigate Programmatically.

--- 22.1-React-Routing/react-routing/src/pages/HomePage.jsx
+++ 22.1-React-Routing/react-routing/src/pages/HomePage.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h2>My Home Page</h2>
       <p>
         Go to <Link to="/products">The list of the product.</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
