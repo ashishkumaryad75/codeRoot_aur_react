e40e768fca93ab2f86076fb549f420d277e98221
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-13 23:18:54 +0530

    Navigateing between Pages with Links.

--- 22.1-React-Routing/react-routing/src/App.jsx
+++ 22.1-React-Routing/react-routing/src/App.jsx
@@ -1,26 +1,26 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  createRoutesFromElements,
-  Route,
+  // createRoutesFromElements,
+  // Route,
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 
 function App() {
-  const routeDefenition = createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/products" element={<ProductPage />} />
-    </Route>
-  );
+  // const routeDefenition = createRoutesFromElements(
+  //   <Route>
+  //     <Route path="/" element={<HomePage />} />
+  //     <Route path="/products" element={<ProductPage />} />
+  //   </Route>
+  // );
 
-  // const router = createBrowserRouter([
-  //   { path: "/", element: <HomePage /> },
-  //   { path: "/products", element: <ProductPage /> },
-  // ]);
+  const router = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "/products", element: <ProductPage /> },
+  ]);
 
-  const router = createBrowserRouter(routeDefenition);
+  // const router = createBrowserRouter(routeDefenition);
 
   return (
     <>
--- 22.1-React-Routing/react-routing/src/pages/HomePage.jsx
+++ 22.1-React-Routing/react-routing/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <>
       <h2>My Home Page</h2>
+      <p>
+        Go to <Link to="/products">The list of the product.</Link>
+      </p>
     </>
   );
 }
