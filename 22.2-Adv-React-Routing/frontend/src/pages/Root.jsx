e63bb0e131c24cce5dbb6f7deaa6c4bfd2ca57cf
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-15 21:50:22 +0530

    More Loader() Data usage.

--- 22.2-Adv-React-Routing/frontend/src/pages/Root.jsx
+++ 22.2-Adv-React-Routing/frontend/src/pages/Root.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router";
 import MainNavigation from "../components/MainNavigation.jsx";
+// import { useLoaderData } from "react-router-dom";
 
 function RootLayout() {
+  // const events = useLoaderData();
+  // console.log("events",events);// useLoaderData is not accessible on this level
+  
   return (
     <>
       <MainNavigation />
