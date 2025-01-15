094b125691fac2fff93043f7147dbba4097ef5ef
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-16 03:00:02 +0530

    Working with action() Functions'

--- 22.2-Adv-React-Routing/frontend/src/pages/NewEvent.jsx
+++ 22.2-Adv-React-Routing/frontend/src/pages/NewEvent.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import EventForm from "../components/EventForm";
+import { redirect } from "react-router-dom";
 
 function NewEventPage() {
-
   // function submitHandler(event){
   //   event.preventDefault();
   // }
 
   return (
     <>
+      <h1>Create a New Event</h1>
       <EventForm />
     </>
   );
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+
+  const eventData = {
+    title: formData.get("title"),
+    image: formData.get("image"),
+    date: formData.get("date"),
+    description: formData.get("description"),
+  };
+
+  // console.log("form data:", eventData);
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    return json({ message: "Could not save event." }, { status: 500 });
+  }
+  return redirect("/events");
+}
