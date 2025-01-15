b4758a8d99107cb3929d340dd9a38f574c9430fe
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-16 00:31:49 +0530

    Reflecting the current NAvigation State in the UI.

--- 22.2-Adv-React-Routing/frontend/src/pages/Root.jsx
+++ 22.2-Adv-React-Routing/frontend/src/pages/Root.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import MainNavigation from "../components/MainNavigation.jsx";
 // import { useLoaderData } from "react-router-dom";
 
 function RootLayout() {
   // const events = useLoaderData();
   // console.log("events",events);// useLoaderData is not accessible on this level
-  
+
+  // const navigation = useNavigation();
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading ....</p>} */}
         <Outlet />
       </main>
     </>
