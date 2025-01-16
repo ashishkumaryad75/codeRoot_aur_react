f2940e7ffdcbe3d5886b0125fd597c183240c244
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-16 15:54:46 +0530

    finalize the react router dom section.

--- 22.2-Adv-React-Routing/frontend/src/pages/Home.jsx
+++ 22.2-Adv-React-Routing/frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import PageContent from "../components/PageContent";
 
 function HomePage() {
   return (
     <>
-      <h1>HomePage</h1>
+      <PageContent title="Welcome!!">
+        <p>Browse all our amazing events!!</p>
+      </PageContent>
     </>
   );
 }
