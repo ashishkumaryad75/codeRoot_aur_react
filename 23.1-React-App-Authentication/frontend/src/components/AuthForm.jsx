02169abbf3e73fba240a0a4ca4c42d4e19acd124
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-20 19:04:42 +0530

    Validating user Input and Outputting Validations Errors.

--- 23.1-React-App-Authentication/frontend/src/components/AuthForm.jsx
+++ 23.1-React-App-Authentication/frontend/src/components/AuthForm.jsx
@@ -1,22 +1,40 @@
 // import { useState } from "react";
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
   const [searchParams] = useSearchParams();
-  // const [isLogin, setIsLogin] = useState(true);
 
+  const data = useActionData();
+  const navigation = useNavigation();
+
+  // const [isLogin, setIsLogin] = useState(true);
   // function switchAuthHandler() {
   //   setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
   // }
 
   const isLogin = searchParams.get("mode") === "login";
+  const isSubmitting = navigation.state === "submitting";
 
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input
@@ -44,7 +62,9 @@ function AuthForm() {
           {/* <button onClick={switchAuthHandler} type="button">
             {isLogin ? "Create new user" : "Login"}
           </button> */}
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting.." : "Save"}
+          </button>
         </div>
       </Form>
     </>
