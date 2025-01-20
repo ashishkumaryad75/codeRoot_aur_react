9d8ced3a42edeb3221818f8a7f791b76f4e8dbcd
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-20 22:00:59 +0530

    Addign Automatic Logout .

--- 23.1-React-App-Authentication/frontend/src/pages/Root.jsx
+++ 23.1-React-App-Authentication/frontend/src/pages/Root.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { Outlet, useNavigation } from "react-router";
+import React, { useEffect } from "react";
+import { Outlet, useLoaderData, useNavigation, useSubmit } from "react-router";
 import MainNavigation from "../components/MainNavigation.jsx";
 // import { useLoaderData } from "react-router-dom";
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
+
   // const events = useLoaderData();
   // console.log("events",events);// useLoaderData is not accessible on this level
 
