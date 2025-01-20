2eae7b699fff37719ac9e1ffa7c4e3f047a8cacc
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-20 17:28:36 +0530

    working with query params

--- 23.1-React-App-Authentication/frontend/src/components/AuthForm.jsx
+++ 23.1-React-App-Authentication/frontend/src/components/AuthForm.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
-import { Form } from "react-router-dom";
+// import { useState } from "react";
+import { Form, Link, useSearchParams } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [searchParams] = useSearchParams();
+  // const [isLogin, setIsLogin] = useState(true);
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  // function switchAuthHandler() {
+  //   setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
+  // }
+
+  const isLogin = searchParams.get("mode") === "login";
 
   return (
     <>
@@ -16,16 +19,31 @@ function AuthForm() {
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
         <p>
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required autoComplete="current-email"/>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            autoComplete="current-email"
+          />
         </p>
         <p>
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" required autoComplete="current-password" />
-          </p>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            autoComplete="current-password"
+          />
+        </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
-          </button>
+          </Link>
+          {/* <button onClick={switchAuthHandler} type="button">
+            {isLogin ? "Create new user" : "Login"}
+          </button> */}
           <button>Save</button>
         </div>
       </Form>
