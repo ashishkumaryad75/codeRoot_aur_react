14383c27290dff1a4387e95e10e25af1960e05ce
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-21 18:47:46 +0530

    Grouping the Tests Together With Test Suites.

--- 31.1-Unit-Testing-React-Apps/frontend/src/components/Greeting.test.jsx
+++ 31.1-Unit-Testing-React-Apps/frontend/src/components/Greeting.test.jsx
@@ -1,13 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders hello word as a text", () => {
-  // Arange
-  render(<Greeting />);
-  
-  // Act
-  const helloWorldElement = screen.getByText('Hello World!!',{exact :false});
 
-  // Assert
-  expect(helloWorldElement).toBeInTheDocument();
-});
+describe('Greeting component',()=>{
+    test("renders hello word as a text", () => {
+      // Arange
+      render(<Greeting />);
+    
+      // Act
+      const helloWorldElement = screen.getByText('Hello World!!',{exact :false});
+    
+      // Assert
+      expect(helloWorldElement).toBeInTheDocument();
+    });
+
+})
