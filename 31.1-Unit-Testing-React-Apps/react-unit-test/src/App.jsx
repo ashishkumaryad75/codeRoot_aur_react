2b84700844ba88d62d41db2498b6f6271362b581
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-21 17:38:57 +0530

    basic setup and initialization of react testing using vite.

---
+++ 31.1-Unit-Testing-React-Apps/react-unit-test/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Button from "./component/Button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h3>App</h3>
+    </>
+  );
+}
+
+export default App;
+
+
+
