169b828c58908aa2c57ccb425098f0502af1953f
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-22 18:09:24 +0530

    Exploring the base types.

---
+++ 32.1-React-Typescipt/basic.ts
@@ -0,0 +1,15 @@
+// Primitives : number, string, boolean
+// More Complex type : Array , object
+// Function Type , parameters
+
+// Primitives
+
+let age : number ;
+age = 12;
+
+let userName : string ;
+userName = "Root";
+
+let isInstructor: boolean;
+isInstructor= true;
+
