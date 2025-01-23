a74de6ae12f4f7808042036bb53655887a2cb100
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-24 04:01:08 +0530

    Adding Event Listeners.

--- 33.1-React-Core-Feature-Walkthrough/react-crash-course/src/components/NewPost.jsx
+++ 33.1-React-Core-Feature-Walkthrough/react-crash-course/src/components/NewPost.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import classes from "./NewPost.module.css";
+import { useState } from "react";
 
 function NewPost() {
+  //    const [] = useState();
+  function changeBodyHandler(event) {
+    console.log(event.target.value);
+  }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
