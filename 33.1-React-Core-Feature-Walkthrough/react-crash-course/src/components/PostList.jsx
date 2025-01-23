640331869a99011e46da9dfd248fc2ef59a0dde1
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-24 04:50:33 +0530

    Handling the Form Submission.

--- 33.1-React-Core-Feature-Walkthrough/react-crash-course/src/components/PostList.jsx
+++ 33.1-React-Core-Feature-Walkthrough/react-crash-course/src/components/PostList.jsx
@@ -2,34 +2,21 @@ import React from "react";
 import classes from "./PostList.module.css";
 import Post from "./Post";
 import NewPost from "./NewPost";
-import { useState } from "react";
 import Modal from "./Modal";
 
 function PostList({isPosting,onStopPosting}) {
-  const [enteredBody, setEntredBody] = useState("");
-  const [enteredAuthor, setEntredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEntredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEntredAuthor(event.target.value);
-  }
+  
 
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
           <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
             onCancel={onStopPosting}
           />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredBody} body={enteredAuthor} />
         <Post author="Root" body="Learning the React App." />
       </ul>
     </>
