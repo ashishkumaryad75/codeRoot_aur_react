fa36806fb7a2b0cd97ad80cbef6c1d7efc16ae40
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-23 16:28:45 +0530

    Working With the Components and TypeScript.

--- 32.2-React-Typescipt/frontend/src/App.tsx
+++ 32.2-React-Typescipt/frontend/src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import Todos from './components/Todos.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"App-logo"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+    <Todos/>
+     
+    </>
   );
 }
 
---
+++ 32.2-React-Typescipt/frontend/src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Todos() {
+  return (
+    <>
+      <ul>
+        <li>Learn React App</li>
+        <li>Learn Typescript</li>
+      </ul>
+    </>
+  );
+}
+
+export default Todos;
