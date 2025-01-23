5587f93a0ffa61b32ef6e55cdfffce5ae3afd6a2
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-23 17:37:32 +0530

    Adding a Data model

--- 32.2-React-Typescipt/frontend/src/App.tsx
+++ 32.2-React-Typescipt/frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import React from "react";
 import Todos from "./components/Todos.tsx";
+import Todo from "./models/todo.ts";
 
 function App() {
-  const todos: string[] = ["Learn React App", "Learn TypeScript App"];
+  const todos = [new Todo("Learn React And"), new Todo("Learn TypeScript")];
   return (
     <>
       <Todos items={todos} />
--- 32.2-React-Typescipt/frontend/src/components/Todos.tsx
+++ 32.2-React-Typescipt/frontend/src/components/Todos.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import Todo from "../models/todo.ts";
 
-const  Todos:React.FC<{items:string[]}> = (props) =>{
-  return (
-    <>
-      <ul>{props.items.map((item)=><li key={item}>{item}</li>)}
-       
-      </ul>
-    </>
-  );
-}
+
+const  Todos:React.FC<{items:Todo[]}> = (props) =>{
+    return (
+      <>
+        <ul>{props.items.map((item)=><li key={item.id}>{item.text}</li>)}
+         
+        </ul>
+      </>
+    );
+  }
+//   const  Todos:React.FC<{items:string[]}> = (props) =>{
+//     return (
+//       <>
+//         <ul>{props.items.map((item)=><li key={item}>{item}</li>)}
+         
+//         </ul>
+//       </>
+//     );
+//   }
 
 export default Todos;
