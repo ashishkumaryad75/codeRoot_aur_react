75e78fd804892141a4c058559b06db1b494ffd41
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-24 03:55:52 +0530

    Preparing the App For State Management.

--- 33.1-React-Core-Feature-Walkthrough/react-crash-course/src/components/PostList.jsx
+++ 33.1-React-Core-Feature-Walkthrough/react-crash-course/src/components/PostList.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./PostList.module.css";
 import Post from "./Post";
+import NewPost from "./NewPost";
 
 function PostList() {
   return (
     <>
+      <NewPost />
       <ul className={classes.posts}>
         <Post author="Coding" body="Learning the frontend." />
         <Post author="Root" body="Learning the React App." />
