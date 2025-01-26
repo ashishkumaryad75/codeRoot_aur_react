ee89375dd1eac1028325d5b7562e99315369bb72
Atul Kumar Yadav <ashish@Atuls-MacBook-Air.local>
2025-01-26 18:05:56 +0530

    Adding Routes and Working with Layout Routes.

--- 33.1-React-Core-Feature-Walkthrough/react-crash-course/src/main.jsx
+++ 33.1-React-Core-Feature-Walkthrough/react-crash-course/src/main.jsx
@@ -2,10 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NewPost from "./components/NewPost.jsx";
+import RootLayout from "./components/RootLayout.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/create-post", element: <NewPost /> ,},
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
